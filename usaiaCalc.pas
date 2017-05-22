e2cceba9490b6ddeb17465036e303455bcdd89f1
Ryan Potts <rpottsoh@gmail.com>
2017-05-22 16:57:33 -0400

    update to correct tire to machine calculation
    The second calculation in the iterative loop was dividing when it should have been multiplying.  Also added another function which will translate machine angles back to tire angles, useful for verifying the primary calculation.

--- usaiaCalc.pas
+++ usaiaCalc.pas
@@ -4,21 +4,22 @@ interface
 
 type
   TTuple<T1, T2> = record
-    Value1: T1;
-    Value2: T2;
-    constructor create(aValue1: T1; aValue2: T2);
+    SAval: T1;
+    IAval: T2;
+    constructor create(aSAval: T1; aIAval: T2);
   end;
 
  function Determine_SAo_IAo(dSA, dIA: Double): TTuple<Double, Double>;
+ function Determine_dSA_dIA(aMachine: TTuple<double, double>): TTuple<double, double>;
 
 
 implementation
 uses System.Math;
 
-constructor TTuple<T1, T2>.Create(aValue1: T1; aValue2: T2);
+constructor TTuple<T1, T2>.Create(aSAval: T1; aIAval: T2);
 begin
-  Value1 := aValue1;
-  Value2 := aValue2;
+  SAval := aSAval;
+  IAval := aIAval;
 end;
 
 function Determine_SAo_IAo(dSA, dIA: Double): TTuple<Double, Double>;
@@ -36,12 +37,37 @@ begin
   for i := 1 to 10 do        // Iterative calculation
   begin
     aIArad := ArcTan(Tan(dIArad) / Cos(aSArad));
-    aSArad := ArcTan(Tan(dSArad) / Cos(aIArad));
+    aSArad := ArcTan(Tan(dSArad) * Cos(aIArad));
   end;
   aSA := aSArad * 180 / pi;
   aIA := aIArad * 180 / pi;
   result := TTuple<double, double>.Create(aSA, aIA);
 end;
 
+function Determine_dSA_dIA(aMachine: TTuple<double, double>): TTuple<double, double>;
+var dSA,
+    dIA: double;
+
+    aSA,
+    aIA: double;
+
+    aSArad,
+    aIArad,
+    dSArad,
+    dIArad: double;
+begin
+  aSA := aMachine.SAval;
+  aIA := aMachine.IAval;
+  aSArad := aSA * pi / 180;
+  aIArad := aIA * pi / 180;
+
+  dSArad := ArcTan(Tan(aSArad) / Cos(aIArad));
+  dIArad := ArcTan(Tan(aIArad) * Cos(aSArad));
+
+  dSA := dSArad * 180 / pi;
+  dIA := dIArad * 180 / pi;
+
+  result := TTuple<double, double>.Create(dSA, dIA);
+end;
 
 end.
